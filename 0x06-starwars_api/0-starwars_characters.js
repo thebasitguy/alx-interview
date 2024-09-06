a628b706fc178e0d9eddbdbb16a51206d55ce469
thebasitguy <mailbasitgh@gmail.com>
2024-09-05 17:08:29 -0700

    task files

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/* Prints all Casts of Star Wars movie */
+
+const request = require('request');
+const starWarsAPI = 'https://swapi-api.alx-tools.com/api/';
+const endPoint = 'films/';
+const movieID = process.argv[2].toString();
+
+request(starWarsAPI + endPoint + movieID, function (error, _, body) {
+  if (error) console.error(error);
+  const objects = JSON.parse(body);
+  const casts = objects.characters;
+  Printresult(casts);
+});
+
+/* requests for each character and prints out
+the cast reculsively and synchronously */
+function Printresult (casts, counter = 0) {
+  request(casts[counter], function (error, _, body) {
+    if (error) console.error(error);
+    console.log(JSON.parse(body).name);
+    if (++counter < casts.length) {
+      Printresult(casts, counter++);
+    }
+  });
+}
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,4 @@
+0x06-starwars_api
+
+Task by:
+Abdul Basit Musah
